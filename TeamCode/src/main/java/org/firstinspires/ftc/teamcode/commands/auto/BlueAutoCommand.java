2574826549602e7e1618001c5115877d20210559
Javier Irizarry-Delgado <92115922+Shxl2@users.noreply.github.com>
2024-11-14 01:50:00 -0500

    Got wpilib installed in an FTC project

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/auto/BlueAutoCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/auto/BlueAutoCommand.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.commands.auto;
 
-import com.arcrobotics.ftclib.command.SequentialCommandGroup;
-
 import org.firstinspires.ftc.teamcode.subsystems.drivetrain.DrivetrainSubsystem;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class BlueAutoCommand extends SequentialCommandGroup {
     public BlueAutoCommand(DrivetrainSubsystem drive) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrain/DrivetrainSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/drivetrain/DrivetrainSubsystem.java
@@ -1,16 +1,16 @@
 package org.firstinspires.ftc.teamcode.subsystems.drivetrain;
 
-import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DrivetrainSubsystem extends SubsystemBase {
     private final Telemetry telemetry;
 
     public DrivetrainSubsystem(HardwareMap hardwareMap, Telemetry telemetry, boolean isFieldCentric) {
         this.telemetry = telemetry;
-
     }
 
     @Override
